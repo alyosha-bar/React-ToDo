2fce82f73a7897bdbfc07838bab40ee1e11c0dd3
Alyosha Baryshnikov <alohahoy@gmail.com>
2023-06-11 21:37:11 +0100

    add some styles and main content adjust

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,13 @@
-
+import '../styles/main.css'
 
 function MainContent() {
-    return (
+    return ( <div className='main-part'>
         <h1> This is the Main content </h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Recusandae dolorum obcaecati optio atque mollitia facilis modi, 
+            quasi nihil dolores fugit perspiciatis omnis quas beatae quis 
+            provident distinctio? Consectetur, nisi quas!</p>
+    </div>
     )
 }
 
