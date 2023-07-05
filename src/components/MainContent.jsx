3f491ccc85d3d531b6205052a83f9782a8326480
Alyosha Baryshnikov <alohahoy@gmail.com>
2023-07-05 20:20:55 +0100

    start work on image carousel

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,11 +4,19 @@ function MainContent() {
     return ( 
     <>
         <section className='red'>
-            <h1>Nice Curves</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos laboriosam magni 
-                amet expedita ex! Sunt dolores laudantium, maxime blanditiis dolor consequatur culpa accusamus 
-                vitae incidunt necessitatibus corrupti distinctio debitis esse.
-            </p>
+            <h1 className='title'> About</h1>
+                 <p className='content'>We are a marketing communications agency which takes pleasure in connecting clients
+                     with their target audiences. Our aim is to develop long-term partnerships with our 
+                     clients by delivering excellent business results: higher profile for their names, brands, 
+                     products and services; higher sales, higher footfall, better profit margins. </p>
+
+                 <br />
+                 <p className='content'>
+                     We offer long-term experience in marketing communications plus insights based on 
+                    personal relationships with leading writers, editors, publishers, bloggers and broadcasters. 
+                    We rely on creativity to achieve stand-out and carefully-composed storylines to achieve consumer 
+                     engagement. Our programmes combine traditional media with online and social media, 
+                     direct-contact events, ambient marketing and endorsement by key opinion formers.</p>
             <div className="wave-cascade">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" 
@@ -21,7 +29,7 @@ function MainContent() {
             </div>
         </section>
         <section className='blue'>
-            <h1>Nice Curves</h1>
+            <h1 className='title' >Nice Curves</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos laboriosam magni 
                 amet expedita ex! Sunt dolores laudantium, maxime blanditiis dolor consequatur culpa accusamus 
                 vitae incidunt necessitatibus corrupti distinctio debitis esse.
@@ -33,16 +41,30 @@ function MainContent() {
                 </svg>
             </div>
         </section>
+
+
         <section className='green'> 
-            <h1>Nice Curves</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos laboriosam magni 
-                amet expedita ex! Sunt dolores laudantium, maxime blanditiis dolor consequatur culpa accusamus 
-                vitae incidunt necessitatibus corrupti distinctio debitis esse.
-            </p>
-            <div className="wave">
+            
+
+            <h1 className='title'> Clients </h1>
+            <img src="../img/avastLogo.png" alt="" />
+
+            <div className="tilt">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
-                    className="shape-fill"></path>
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
+                </svg>
+            </div>
+        </section>
+
+
+        <section>
+            <h1> Sample </h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, dolore ipsa dicta aliquam 
+                explicabo quas magnam repellat, beatae repudiandae fugit vel fuga, accusantium aliquid 
+                sequi optio. Ipsa numquam sint voluptatem?</p>
+            <div className="tilt green-fill">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
                 </svg>
             </div>
         </section>
